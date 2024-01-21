b9bf74115c733b9cc2a7b10b87287035d4b4c30f
Bryan David Alvarez Galvis <bryanalvarezg.kcts@gmail.com>
2024-01-21 15:33:35 -0500

    typescript

--- src/scripts/constants/packagejson.ts
+++ src/scripts/constants/packagejson.ts
@@ -17,6 +17,9 @@ export const pkg = {
 
 export const pkgJsonCommon = {
   ...pkg,
+  scripts: {
+    dev: 'node --watch src/index.js'
+  },
   devDependencies: {
     standard: '^17.1.0'
   },
@@ -29,4 +32,28 @@ export const pkgJsonCommon = {
 export const pkgJsonModule = {
   ...pkgJsonCommon,
   type: 'module'
+}
+
+export const pkgJsonTs = {
+  ...pkg,
+  main: 'src/index.ts',
+  scripts: {
+    build: 'tsc',
+    dev: 'ts-node-dev src/index.ts'
+  },
+  devDependencies: {
+    'ts-standard': '^12.0.2',
+    typescript: '^5.3.3',
+    '@types/express': '^4.17.21'
+  },
+  eslintConfig: {
+    extends: './node_modules/ts-standard/eslintrc.json',
+    parserOptions: {
+      project: './tsconfig.json'
+    },
+    rules: {
+      '@typescript-eslint/no-misused-promises': 'off'
+    }
+  }
+
 }
--- src/scripts/typescript.ts
+++ src/scripts/typescript.ts
@@ -1,1 +1,21 @@
-export const generateTs = (): void => {}
+import { pkgJsonTs } from './constants/packagejson'
+import { join } from 'path'
+import { copy, ensureFile, writeJSON } from 'fs-extra'
+import { dirName } from './getArgs'
+
+export const generateTs = async (): Promise<void> => {
+  const moduleTemplate = join(__dirname, '../templates/typescript')
+  const sourceProject = dirName
+  const packagejsonDir = `${dirName}/package.json`
+
+  try {
+    const index = copy(moduleTemplate, sourceProject)
+    const packagejson = ensureFile(packagejsonDir)
+
+    await index
+    await packagejson
+    await writeJSON(packagejsonDir, pkgJsonTs)
+  } catch (error) {
+    console.log(error)
+  }
+}
